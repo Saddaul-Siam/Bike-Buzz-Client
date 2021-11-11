9180705ba0e0939f35d12f1aec53141f797a58e9
saddaul-siam <saddadul.siam@gmail.com>
2021-11-12 02:52:26 +0600

    banner bug fixed

--- src/pages/Home/Banner/Banner.js
+++ src/pages/Home/Banner/Banner.js
@@ -29,13 +29,14 @@ const Banner = () => {
         <Grid item xs={12} sm={6} md={6} lg={6} >
           <img width="100%" src="https://i.ibb.co/fXkPw6g/banner.png" alt="" />
         </Grid>
-        <Grid item xs={12} sm={6} md={6} lg={6} sx={{  display: 'flex', alignItems: 'center' }}>
+        <Grid item xs={12} sm={6} md={6} lg={6} sx={{ display: 'flex', alignItems: 'center' }}>
           <Box sx={{ alignItems: 'center' }} >
-            <Typography variant="h2" sx={{ fontWeight: 600 }}>
+            <Typography variant="h3" sx={{ fontWeight: 600 }}>
               EVERYTHING <br /> MOTORSPORTS
             </Typography>
             <Typography variant="body1" sx={{ fontSize: 18, color: 'gray', py: 3 }}>
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam aliquam amet possimus,<br /> nemo quis eaque  quae quaerat earum aspernatur cumque!
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nam aliquam amet possimus, <br />
+              nemo quis eaque  quae quaerat earum aspernatur cumque!
             </Typography>
             <MuiButton>VIEW MORE</MuiButton>
           </Box>
