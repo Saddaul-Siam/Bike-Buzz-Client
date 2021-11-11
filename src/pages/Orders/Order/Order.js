fe71d864db56c2f0d1a7fefd2c6052da36d61af9
saddaul-siam <saddadul.siam@gmail.com>
2021-11-11 17:16:59 +0600

    add order post api

--- src/pages/Orders/Order/Order.js
+++ src/pages/Orders/Order/Order.js
@@ -1,10 +1,88 @@
-import React from 'react';
-
+import { Button, Container, Grid, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { useForm } from "react-hook-form";
+import useAuth from '../../../Hooks/useAuth';
+import { makeStyles } from '@mui/styles';
+import Navigation from '../../Shared/Navigation/Navigation';
 const Order = () => {
+  const { ordersId } = useParams();
+  const [order, setOrder] = useState({});
+
+
+  const { user } = useAuth()
+  useEffect(() => {
+    fetch(`http://localhost:5000/product/${ordersId}`)
+      .then(res => res.json())
+      .then(data => setOrder(data))
+  }, [ordersId]);
+
+  const { register, handleSubmit,reset, formState: { errors } } = useForm()
+  const onSubmit = data => {
+    data.productName = order.name
+    data.price = order.price
+    console.log(data);
+    fetch(`http://localhost:5000/order`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(data)
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.acknowledged) {
+          alert('order added')
+          reset()
+        }
+        console.log(data);
+      })
+  };
+
+  const useStyle = makeStyles({
+    input: {
+      width: '100%',
+      height: "50px",
+      marginBottom: 15,
+      border: 0,
+      borderBottom: '1px solid black'
+    },
+
+  })
+
+  const { input } = useStyle()
   return (
-    <div>
-      
-    </div>
+    <Container>
+      <Navigation></Navigation>
+      <Typography variant="h4" sx={{ marginTop: 3, fontWeight: 600 }}>Orders pages </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={12} md={6} lg={6}>
+          {order.name}
+        </Grid>
+        <Grid item xs={12} sm={12} md={6} lg={6}>
+
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Typography variant="subtitle" sx={{ fontWeight: 'bold' }}>Product Name</Typography>
+            <input className={input} value={order.name} {...register("productName")} type="text" required />
+
+            <Typography variant="subtitle" sx={{ fontWeight: 'bold' }}>Price</Typography>
+            <input className={input} value={order.price} {...register("price")} type="number" required />
+
+            <Typography variant="subtitle" sx={{ fontWeight: 'bold' }}>Your Name</Typography>
+            <input className={input} defaultValue={user.displayName}  {...register("name")} type="text" required />
+
+            <Typography variant="subtitle" sx={{ fontWeight: 'bold' }}>Email</Typography>
+            <input className={input} defaultValue={user.email} {...register("email")} type="email" required />
+
+            <Typography variant="subtitle" sx={{ fontWeight: 'bold' }}>Address</Typography>
+            <input className={input} placeholder="Your Address"  {...register("address")} type="text" required />
+
+            <Typography variant="subtitle" sx={{ fontWeight: 'bold' }}>City</Typography>
+            <input className={input} placeholder="Your City"   {...register("city")} type="text" required />
+            {errors.exampleRequired && <span>This field is required</span>}
+            <Button className={input} variant="contained" type="submit">Order Now</Button>
+          </form>
+        </Grid>
+      </Grid>
+    </Container>
   );
 };
 
--- src/pages/Orders/Orders/Orders.js
+++ src/pages/Orders/Orders/Orders.js
@@ -1,33 +1,16 @@
-import { Container, Grid, Typography } from '@mui/material';
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router';
+import React from 'react';
+import Footer from '../../Shared/Footer/Footer';
+import Navigation from '../../Shared/Navigation/Navigation';
 import Order from '../Order/Order';
 
 const Orders = () => {
-  const { ordersId } = useParams();
-  const [order, setOrder] = useState({});
-  const { name, img, price, description } = order;
-  // console.log(orders);
-  useEffect(() => {
-    fetch(`http://localhost:5000/product/${ordersId}`)
-      .then(res => res.json())
-      .then(data => setOrder(data))
-  }, [ordersId])
   return (
-    <Container>
-      <Typography variant="h4" sx={{py:3,fontWeight: 600}}>Orders pages </Typography>
-      <Grid container spacing={2}>
-        <Grid item xs={12} sm={12} md={6} lg={6}>
-          {name}
-        </Grid>
-        <Grid item xs={12} sm={12} md={6} lg={6}>
-          {price}
-        </Grid>
-      </Grid>
-    </Container>
+    <>
+      <Navigation></Navigation>
+      <Order></Order>
+      <Footer></Footer>
+    </>
   );
 };
 
 export default Orders;
-
-// {/* <Order dt={data}></Order> */ }
