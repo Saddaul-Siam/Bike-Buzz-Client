df98b4573ca1453ab9a292e8e6974a6b7bbc25a1
saddaul-siam <saddadul.siam@gmail.com>
2021-11-12 02:16:47 +0600

    added env

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBHWRKy77Inw5Cg-HKj-ostBVkT91ewfrM",
-  authDomain: "bike-buzz.firebaseapp.com",
-  projectId: "bike-buzz",
-  storageBucket: "bike-buzz.appspot.com",
-  messagingSenderId: "213135722435",
-  appId: "1:213135722435:web:3f5dbe5245fcb68a035f76"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 export default firebaseConfig;
