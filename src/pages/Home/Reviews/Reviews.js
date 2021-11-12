4b7d1e69292cf3951419a49d42830fab44dd2c2c
saddaul-siam <saddadul.siam@gmail.com>
2021-11-12 17:43:06 +0600

    code add

--- src/pages/Home/Reviews/Reviews.js
+++ src/pages/Home/Reviews/Reviews.js
@@ -1,7 +1,8 @@
 import { Container, Grid } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import Review from '../Review/Review';
-
+import Pagination from '@mui/material/Pagination';
+import Stack from '@mui/material/Stack';
 
 
 const Reviews = () => {
@@ -14,12 +15,16 @@ const Reviews = () => {
 
 
   return (
-    <Container sx={{ mt:10}}>
+    <Container sx={{ mt: 10 }}>
       <Grid container spacing={2}>
         {
           reviews.map((review) => <Review key={review._id} review={review}></Review>)
         }
+
       </Grid>
+      <Stack spacing={2}>
+        <Pagination count={10} color="secondary" />
+      </Stack>
     </Container>
   );
 };
